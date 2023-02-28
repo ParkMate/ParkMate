12f2f3e774e22485dc0dfd6b1435c43ca9b30bf9
teoconte <124182490+teoconte@users.noreply.github.com>
2023-02-28 18:40:41 +0100

    Add files via upload

--- progetto_parcheggio/app.js
+++ progetto_parcheggio/app.js
@@ -1,5 +1,5 @@
-//Register PWA service worker
-/*if ('serviceWorker' in navigator) {
+/*//Register PWA service worker
+if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('sw.js');
 }
 
@@ -11,6 +11,7 @@ if(location.protocol=="http:"){
 
 //codice
 
+
 var n=0;
 var original = document.getElementById("veicolo");
 
@@ -18,30 +19,40 @@ function ID(id){
   return document.getElementById(id);
 }
 
-function apriChiudi(){
-  let veicoli = ID("veicoli");
+function apriChiudi(id,aperto,chiuso){
+  let veicoli = ID(id);
 
-  if(veicoli.className === 'open'){
-    veicoli.classList.remove('open');
-    veicoli.classList.add('close');
+  if(veicoli.className === aperto){
+    veicoli.classList.remove(aperto);
+    veicoli.classList.add(chiuso);
   }
-  else if(veicoli.className === 'close'){
-    veicoli.classList.remove('close');
-    veicoli.classList.add('open');
+  else if(veicoli.className === chiuso){
+    veicoli.classList.remove(chiuso);
+    veicoli.classList.add(aperto);
   }
 
 }
+
+
+
 function addVehicles(){
   let veicolo = document.getElementsByClassName("veicolo");
   let veicoli = document.getElementById("veicoli");
   let divClone = veicolo[0].cloneNode(true);
+  divClone.id="";
+  let nodo = divClone.childNodes;
+  let second = nodo[3].childNodes;
+  let reload = second[1].childNodes;
+  reload[1].id = ++n;
   veicoli.appendChild(divClone);
 }
 
-function getPos() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else {
-    x.innerHTML = "Geolocation is not supported by this browser.";
-  }
+function getPos(id){
+  map = document.getElementsByClassName("map");
+  navigator.geolocation.getCurrentPosition(position =>{
+    const {latitude, longitude} = position.coords;
+    map[Number(id)].innerText="";
+    map[Number(id)].innerHTML = '<iframe  src="https://maps.google.com/maps?q='+latitude+','+longitude+'&amp;z=15&amp;output=embed"></iframe>';
+  });
 }
+
